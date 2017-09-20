a30fecdc6d5f35b020a166c8e16ec418f1cb016d
Chan Yong <chanyong.bj@gmail.com>
2017-09-20 15:05:16 +0800

    update exports.sh

--- bash/exports.bash
+++ bash/exports.bash
@@ -7,36 +7,36 @@ export WORKON_HOME=$HOME/.virtualenvs
 export PIP_VIRTUALENV_BASE=$HOME/.virtualenvs
 export PIP_RESPECT_VIRTUALENV=true
 
-# JDK config
-export JAVA_HOME=/usr/lib/jvm/jdk1.7
+# JDK config, default JDK version 1.8
+export JAVA_HOME=/usr/lib/jvm/jdk1.8
 export JRE_HOME=${JAVA_HOME}/jre
 export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
 export PATH=${JAVA_HOME}/bin:$PATH
 
 # Scala config
-export SCALA_HOME=/usr/share/scala
-export PATH=${SCALA_HOME}/bin:$PATH
+# export SCALA_HOME=/usr/share/scala
+# export PATH=${SCALA_HOME}/bin:$PATH
 
 # Maven
-export MAVEN_HOME=/usr/local/apache-maven/apache-maven-3.1.1
-export PATH=${MAVEN_HOME}/bin:$PATH
+# export MAVEN_HOME=/usr/local/apache-maven/apache-maven-3.1.1
+# export PATH=${MAVEN_HOME}/bin:$PATH
 
 # Hadoop config
-export HADOOP_HOME=/usr/local/hadoop
+# export HADOOP_HOME=/usr/local/hadoop
 
-export HADOOP_MAPRED_HOME=$HADOOP_HOME
-export HADOOP_COMMON_HOME=$HADOOP_HOME
-export HADOOP_HDFS_HOME=$HADOOP_HOME
-export YARN_HOME=$HADOOP_HOME
+# export HADOOP_MAPRED_HOME=$HADOOP_HOME
+# export HADOOP_COMMON_HOME=$HADOOP_HOME
+# export HADOOP_HDFS_HOME=$HADOOP_HOME
+# export YARN_HOME=$HADOOP_HOME
 #export HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop
 #export YARN_CONF_DIR=${HADOOP_HOME}/etc/hadoop
 
-export PATH=${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin:$PATH
+# export PATH=${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin:$PATH
 
 # Hive config
-export HIVE_HOME=/usr/local/hive
-export PATH=${HIVE_HOME}/bin:$PATH
+# export HIVE_HOME=/usr/local/hive
+# export PATH=${HIVE_HOME}/bin:$PATH
 
 # Spark
-export SPARK_HOME=/usr/local/spark
-export PATH=${SPARK_HOME}/bin:${SPARK_HOME}/sbin:$PATH
+# export SPARK_HOME=/usr/local/spark
+# export PATH=${SPARK_HOME}/bin:${SPARK_HOME}/sbin:$PATH
